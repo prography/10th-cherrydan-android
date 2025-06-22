7ae58adaa645084883ee580b181b90edc5692187
최현정 <hjchoi.dev@gmail.com>
2025-06-22 15:08:57 +0900

    feat: Create CherrydanTheme

---
+++ core/presentation/designsystem/src/main/java/com/hyunjung/core/presentation/designsystem/Theme.kt
@@ -0,0 +1,32 @@
+package com.hyunjung.core.presentation.designsystem
+
+import android.app.Activity
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+val LocalCherrydanColors = staticCompositionLocalOf { CherrydanColors }
+val LocalCherrydanTypography = staticCompositionLocalOf { CherrydanTypography }
+
+@Composable
+fun CherrydanTheme(
+    content: @Composable () -> Unit
+) {
+    val colorScheme = CherrydanColors
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+        }
+    }
+
+    CompositionLocalProvider(
+        LocalCherrydanColors provides colorScheme,
+        LocalCherrydanTypography provides CherrydanTypography,
+        content = content
+    )
+}
