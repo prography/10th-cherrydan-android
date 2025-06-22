7a695420b72ca7630a2d0cea607a587a1eab429a
최현정 <hjchoi.dev@gmail.com>
2025-06-22 15:28:38 +0900

    design: Add Icons

---
+++ core/presentation/designsystem/src/main/java/com/hyunjung/core/presentation/designsystem/Icons.kt
@@ -0,0 +1,41 @@
+package com.hyunjung.core.presentation.designsystem
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+
+val CalendarIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_calendar)
+
+val EditIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_edit)
+
+val FilterIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_filter)
+
+val ReturnIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_return)
+
+val ClockIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_clock)
+
+val NotificationIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_notification)
+
+val TrashIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_trash)
+
+val ShareIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_share)
+
+val HamburgerIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_hamburger)
