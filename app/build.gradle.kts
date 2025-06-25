906531f037de4f4c374a5d8672776f5d06dcc8a0
최현정 <hjchoi.dev@gmail.com>
2025-06-25 23:37:28 +0900

    build: Add koin dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     // Crypto
     implementation(libs.androidx.security.crypto.ktx)
 
+    implementation(libs.bundles.koin)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
