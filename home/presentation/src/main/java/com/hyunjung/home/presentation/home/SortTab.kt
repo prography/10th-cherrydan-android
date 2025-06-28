ecda74ce6d748cb1fe5ee46bff41499a43f8e5ad
최현정 <hjchoi.dev@gmail.com>
2025-06-28 18:17:34 +0900

    feat: Add CherrydanTab

---
+++ home/presentation/src/main/java/com/hyunjung/home/presentation/home/SortTab.kt
@@ -0,0 +1,8 @@
+package com.hyunjung.home.presentation.home
+
+enum class SortTab(val label: String) {
+    Popular("인기순"),
+    Latest("최신순"),
+    Deadline("마감임박순"),
+    LowCompetition("경쟁률 낮은순")
+}
