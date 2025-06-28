57a47b976afa2b8c8cca0982a474e55ca5ef6832
최현정 <hjchoi.dev@gmail.com>
2025-06-28 18:11:09 +0900

    feat: Add CherrydanTopAppBar

---
+++ core/presentation/designsystem/src/main/java/com/hyunjung/core/presentation/designsystem/component/CherrydanTopAppBarColors.kt
@@ -0,0 +1,10 @@
+package com.hyunjung.core.presentation.designsystem.component
+
+import androidx.compose.ui.graphics.Color
+
+data class CherrydanTopAppBarColors(
+    val containerColor: Color,
+    val titleColor: Color,
+    val navigationIconColor: Color,
+    val actionIconColor: Color
+)
