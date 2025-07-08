1f49c6539c5f99455547c250c5bacee0002d0b57
최현정 <hjchoi.dev@gmail.com>
2025-07-08 16:09:01 +0900

    fix: Update package names in instrumented tests

--- core/data/src/androidTest/java/com/hyunjung/core/data/ExampleInstrumentedTest.kt
+++ core/data/src/androidTest/java/com/hyunjung/core/data/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hyunjung.core.data.test", appContext.packageName)
+        assertEquals("com.hyunjung.core.data", appContext.packageName)
     }
 }
